c4fc52ce450c25bdf05c6c3c613a5406d8656f24
Semih <76804351+semihuguz@users.noreply.github.com>
2022-04-22 11:07:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            
+            
+            if(time>=6 && time<=11)
+                Console.WriteLine("Gunaydin");
+            else if(time<=18)
+                Console.WriteLine("Iyi Gunler");
+            else
+                Console.WriteLine("Iyi Geceler");
+            string sonuc = time<=18 ? "Iyi gunler!" : "Iyi geceler";
+            sonuc = time >=6 && time<=11 ? "Gunaydin" : time<=18 ? "iyi gunler" :"Iyi geceler";
+            Console.WriteLine(sonuc);
+
+        }
+
+    }
+
+}
